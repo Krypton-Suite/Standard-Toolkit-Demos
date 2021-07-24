13e43f6f9b8600b2510f24fd61927bf5d7c7b893
Smurf-iv <Smurf.iv@gmail.com>
2021-07-24 13:33:21 +0100

    - Add Demo Dark Labels

--- Source/Krypton Toolkit Examples/KryptonForm Examples/Properties/AssemblyInfo.cs
+++ Source/Krypton Toolkit Examples/KryptonForm Examples/Properties/AssemblyInfo.cs
@@ -4,9 +4,9 @@
 //	The software and associated documentation supplied hereunder are the 
 //  proprietary information of Component Factory Pty Ltd, PO Box 1504, 
 //  Glen Waverley, Vic 3150, Australia and are supplied subject to licence terms.
+//  Version 5.550.0 	www.ComponentFactory.com
 // 
 //  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV) 2017 - 2021. All rights reserved. (https://github.com/Krypton-Suite/Standard-Toolkit)
-//  Version 5.550.0 	www.ComponentFactory.com
 // *****************************************************************************
 
 using System;
@@ -30,7 +30,6 @@ using System.Runtime.InteropServices;
 [assembly: StringFreezing]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-[assembly: AllowPartiallyTrustedCallers()]
 [assembly: Dependency("System", LoadHint.Always)]
 [assembly: Dependency("System.Drawing", LoadHint.Always)]
 [assembly: Dependency("System.Windows.Forms", LoadHint.Always)]
