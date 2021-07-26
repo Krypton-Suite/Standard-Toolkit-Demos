f7de7466d4d168fd4e013d7ea81ce23a6c50a415
Peter Wagner <peterwagner@live.co.uk>
2021-07-26 11:02:09 +0100

    * Theme testing

---
+++ Source/Krypton Toolkit Examples/Krypton Theme Playground/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Krypton.Toolkit;
+
+namespace KryptonThemePlayground
+{
+    public partial class Form1 : KryptonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
