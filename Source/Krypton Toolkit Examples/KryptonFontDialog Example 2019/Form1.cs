fbb05ad040ff9b0cb45f4ddc26a8e9ffb3d8f6bd
Smurf-iv <Smurf.iv@gmail.com>
2021-08-13 19:14:33 +0100

    - Create Handler code for the CommonDialog dereived controls - Only replace buttons (Normal and checkbox) - Replace drawing of GroupBox's - Everything else is handled "in place" - Some code tidys as well  #243

---
+++ Source/Krypton Toolkit Examples/KryptonFontDialog Example 2019/Form1.cs
@@ -0,0 +1,66 @@
+﻿using Krypton.Toolkit;
+using Krypton.Toolkit.Controls_Toolkit;
+
+using System;
+
+namespace KryptonFontDialog_Example_2019
+{
+    public partial class Form1 : KryptonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Palette2010Blue_CheckedChanged(object sender, EventArgs e)
+        {
+            KryptonManager.GlobalPaletteMode = PaletteModeManager.Office2010Blue;
+        }
+
+        private void Palette2010Silver_CheckedChanged(object sender, EventArgs e)
+        {
+            KryptonManager.GlobalPaletteMode = PaletteModeManager.Office2010Silver;
+        }
+
+        private void Palette2010Black_CheckedChanged(object sender, EventArgs e)
+        {
+            KryptonManager.GlobalPaletteMode = PaletteModeManager.Office2010Black;
+        }
+
+        private void Palette2007Blue_CheckedChanged(object sender, EventArgs e)
+        {
+            KryptonManager.GlobalPaletteMode = PaletteModeManager.Office2007Blue;
+        }
+
+        private void PaletteSparkleOrange_CheckedChanged(object sender, EventArgs e)
+        {
+            KryptonManager.GlobalPaletteMode = PaletteModeManager.SparkleOrange;
+        }
+
+        private void PaletteProfessional_CheckedChanged(object sender, EventArgs e)
+        {
+            KryptonManager.GlobalPaletteMode = PaletteModeManager.ProfessionalSystem;
+        }
+
+        private void FolderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonShowFontDialog_Click(object sender, EventArgs e)
+        {
+            var kfd = new KryptonFontDialog 
+            {
+                ShowColor = chkShowColour.Checked,
+                ShowHelp = chkShowHelp.Checked,
+                AllowScriptChange = chkAllowScriptChange.Checked,
+                AllowSimulations = chkAllowSimulations.Checked,
+                AllowVectorFonts = chkAllowVectorFonts.Checked,
+                ShowApply = chkShowApply.Checked,
+                ShowEffects = chkShowEffects.Checked
+                
+            };
+            kfd.ShowDialog(this);
+        }
+    }
+}
