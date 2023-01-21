fff194bca86c6f2b60ba392d730a49209ccc00de
Peter Wagner <peterwagner@live.co.uk>
2023-01-21 10:52:11 +0000

    * Demonstrates the usage of a `KryptonPropertyGrid`

---
+++ Source/Krypton Toolkit Examples/KryptonPropertyGridExample/Program.cs
@@ -0,0 +1,28 @@
+#region BSD License
+/*
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2017 - 2023. All rights reserved. 
+ *  
+ */
+#endregion
+
+using System;
+using System.Windows.Forms;
+
+namespace KryptonPropertyGridExample
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
