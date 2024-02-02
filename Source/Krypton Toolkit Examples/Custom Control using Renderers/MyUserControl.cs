950574fd9bd736cb4b5519a95bc99b6928d48b0f
tobitege <tobitege@gmx.de>
2024-02-02 12:10:42 +0100

    MyUserControl: fix LayoutContent call
    method call with invalid number of parameters

--- Source/Krypton Toolkit Examples/Custom Control using Renderers/MyUserControl.cs
+++ Source/Krypton Toolkit Examples/Custom Control using Renderers/MyUserControl.cs
@@ -168,7 +168,7 @@ namespace CustomControlUsingRenderers
                 // Ask the renderer to work out how the Content values will be laid out and
                 // return a memento object that we cache for use when actually performing painting
                 _mementoContent = renderer.RenderStandardContent.LayoutContent(viewContext, innerRect,  _paletteContent, 
-                    this, Orientation, buttonState, false, false);
+                    this, Orientation, buttonState);
             }
 
             base.OnLayout(e);
